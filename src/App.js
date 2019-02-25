8a58a96567d40e649a3e458d26b0cbd243bf0d11
Damien D <durandamien1997@gmail.com>
2019-02-24 23:14:03 -0700

    Added functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 
 class App extends Component {
 	handleClick = (e) => {
-		alert(e.target.innerText);
+		const screen = document.getElementById("screen");
+		const update = e.target.innerText;
+		screen.value += update;
+	}
+
+	handleDelete = () => {
+		const screen = document.getElementById("screen");
+		screen.value = screen.value.slice(0, -1);
+	}
+
+	handleEval = () => {
+		const screen = document.getElementById("screen");
+		const eq = screen.value;
+		const update = eval(eq);
+		screen.value = update;
+
 	}
 
 	render() {
 		return (
 			<div className="container">
-				<input type="text" className="screen" disabled />
+				<input type="text" id="screen" disabled />
 				<div className="left-inputs">
 					<div className="left-row">
 						<div id="7" onClick={this.handleClick} className="button">7</div>
@@ -28,18 +43,18 @@ class App extends Component {
 					<div className="left-row">
 						<div id="0" onClick={this.handleClick} className="button">0</div>
 						<div id="decimal" onClick={this.handleClick} className="button">.</div>
-						<div id="sum" onClick={this.handleClick} className="button">=</div>
+						<div id="eval" onClick={this.handleEval} className="button">=</div>
 					</div>
 				</div>
 				<div className="right-inputs">
 					<div className="right-row">
-						<div id="delete" onClick={this.handleClick} className="button right">{"<"}</div>
+						<div id="delete" onClick={this.handleDelete} className="button right">del</div>
 					</div>
 					<div className="right-row">
 						<div id="divide" onClick={this.handleClick} className="button right">/</div>
 					</div>
 					<div className="right-row">
-						<div id="multiply" onClick={this.handleClick} className="button right">x</div>
+						<div id="multiply" onClick={this.handleClick} className="button right">*</div>
 					</div>
 					<div className="right-row">
 						<div id="subtract" onClick={this.handleClick} className="button right">-</div>
